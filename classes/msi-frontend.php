d92f2b09fed5fbdd8e0402daa4319ce29597eca3
Paul Clark <pdclark@brainstormmedia.com>
2013-11-24 00:32:38 -0900

    Fix horizontal scrollbar on RTL layouts.

--- classes/msi-frontend.php
+++ classes/msi-frontend.php
@@ -189,6 +189,7 @@ class MSI_Frontend {
 		<style>
 			/* Accessible for screen readers but hidden from view */
 			.fa-hidden { position:absolute; left:-10000px; top:auto; width:1px; height:1px; overflow:hidden; }
+			.rtl .fa-hidden { left:10000px; }
 			.fa-showtext { margin-right: 5px; }
 		</style>
 		<?php
